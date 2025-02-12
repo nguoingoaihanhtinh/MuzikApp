ac90bbbb1c83a3a1118da573fdec92360e282827
nguoingoaihanhtinh <22520688@gm.uit.edu.vn>
2025-02-12 14:06:46 +0700

    playlist fe

---
+++ client/src/app/(app)/playlist/_components/PlayList.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const PlayList = () => {
+  return (
+    <div className="h-fit flex w-[90%] ">
+      <h2 className="text-2xl font-bold mb-4">
+        Your <span className="text-pink-500">Playlists</span>
+      </h2>
+    </div>
+  );
+};
+
+export default PlayList;
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -14,6 +14,8 @@ import {
   FaCog,
   FaPersonBooth,
   FaSignOutAlt,
+  FaBars,
+  FaTimes,
 } from "react-icons/fa";
 import { useUser } from "@/contexts/UserContext";
 import { useLoading } from "@/contexts/LoadingContext";
@@ -21,6 +23,7 @@ import { IconType } from "react-icons/lib";
 import AddPlaylistModal from "./AddPlaylistModal";
 import { Playlist } from "@/types/global";
 import { getMyPlaylists } from "@/actions/playlist-actions";
+import PlaylistSideBar from "./Album/PlaylistSideBar";
 
 export type SidebarItem = {
   name: string;
@@ -65,12 +68,12 @@ const Sidebar = ({ paddingBottom = "0" }) => {
   const { logout, isAuthenticated } = useUser();
   const { setLoadingState } = useLoading();
   const [isAddPlaylistModalOpen, setIsAddPlaylistModalOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [playlists, setPlaylists] = useState<Playlist[]>([]);
   const [favorites, setFavorites] = useState<Playlist[]>([]);
   const pathname = usePathname();
   const router = useRouter();
   useEffect(() => {
-    console.log("hellop");
     async function fetchPlaylists() {
       try {
         const playlistData = await getMyPlaylists();
@@ -90,114 +93,125 @@ const Sidebar = ({ paddingBottom = "0" }) => {
     router.push("/login");
     setLoadingState(false);
   };
-
+  if (pathname.startsWith("/playlist/")) {
+    return <PlaylistSideBar />;
+  }
   return (
-    <div
-      className={`h-screen sticky top-0 animated-border z-2 flex-col min-h-screen text-white max-w-fit p-6 border-r border-r-general-pink shadow-[8px_0px_24.2px_0px_rgba(238,16,176,0.15)] animate-[border-pulse_2s_ease-in-out_infinite] ${isAuthenticated ? `flex` : `hidden`}`}
-    >
-      <div
-        className="overflow-y-scroll"
-        style={{
-          paddingBottom,
-        }}
+    <>
+      {/* Toggle Button */}
+      <button
+        className="fixed top-4 left-4 z-50 bg-gray-800 text-white p-2 rounded-full shadow-lg"
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
       >
-        {/* Logo */}
-        <h1
-          onClick={() => {
-            router.push("/home");
+        {isSidebarOpen ? <FaTimes size={20} /> : <FaBars size={20} />}
+      </button>
+      <div
+        className={`h-screen sticky top-0 animated-border z-2 flex-col min-h-screen text-white max-w-fit p-6 border-r border-r-general-pink shadow-[8px_0px_24.2px_0px_rgba(238,16,176,0.15)] animate-[border-pulse_2s_ease-in-out_infinite] ${isAuthenticated ? (isSidebarOpen ? "flex" : "hidden") : "hidden"}`}
+      >
+        <div
+          className="overflow-y-scroll"
+          style={{
+            paddingBottom,
           }}
-          className="title-text-gradient mb-8 cursor-pointer"
         >
-          Lora
-        </h1>
+          {/* Logo */}
+          <h1
+            onClick={() => {
+              router.push("/home");
+            }}
+            className="title-text-gradient mb-8 cursor-pointer"
+          >
+            Lora
+          </h1>
 
-        {sidebarSections.map((section, index) => (
-          <div key={section.title} className={index !== sidebarSections.length - 1 ? "mb-4" : ""}>
-            <h3 className="sidebar-second-header tracking-wide mb-3 whitespace-nowrap">{section.title}</h3>
+          {sidebarSections.map((section, index) => (
+            <div key={section.title} className={index !== sidebarSections.length - 1 ? "mb-4" : ""}>
+              <h3 className="sidebar-second-header tracking-wide mb-3 whitespace-nowrap">{section.title}</h3>
+              <ul>
+                {section.items.map((item) => (
+                  <li key={item.name} className="flex flex-col">
+                    {item.name === "Logout" ? (
+                      <button onClick={handleLogout} className={`group flex items-center space-x-3 p-2`}>
+                        <item.icon
+                          className={`text-xs group-hover:text-pink-500 ${pathname === item.href ? "text-general-pink" : ""}`}
+                        />
+                        <span
+                          className={`sub-header whitespace-nowrap group-hover:text-pink-500 ${pathname === item.href ? "text-general-pink" : ""}`}
+                        >
+                          {item.name}
+                        </span>
+                      </button>
+                    ) : (
+                      <Link
+                        href={item.href}
+                        className={`group flex items-center space-x-3 hover:bg-slate-400/20 p-2 rounded-md`}
+                      >
+                        <item.icon
+                          className={`text-xs group-hover:text-pink-500 ${pathname === item.href ? "text-general-pink" : ""}`}
+                        />
+                        <span
+                          className={`sub-header whitespace-nowrap group-hover:text-pink-500 ${pathname === item.href ? "text-general-pink" : ""}`}
+                        >
+                          {item.name}
+                        </span>
+                      </Link>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+
+          {/* Playlist and Favorites Section */}
+          <div className="mb-4">
+            <h3 className="sidebar-second-header tracking-wide mb-3 whitespace-nowrap">Playlist and Favorite</h3>
             <ul>
-              {section.items.map((item) => (
-                <li key={item.name} className="flex flex-col">
-                  {item.name === "Logout" ? (
-                    <button onClick={handleLogout} className={`group flex items-center space-x-3 p-2`}>
-                      <item.icon
-                        className={`text-xs group-hover:text-pink-500 ${pathname === item.href ? "text-general-pink" : ""}`}
-                      />
-                      <span
-                        className={`sub-header whitespace-nowrap group-hover:text-pink-500 ${pathname === item.href ? "text-general-pink" : ""}`}
-                      >
-                        {item.name}
-                      </span>
-                    </button>
-                  ) : (
-                    <Link
-                      href={item.href}
-                      className={`group flex items-center space-x-3 hover:bg-slate-400/20 p-2 rounded-md`}
-                    >
-                      <item.icon
-                        className={`text-xs group-hover:text-pink-500 ${pathname === item.href ? "text-general-pink" : ""}`}
-                      />
-                      <span
-                        className={`sub-header whitespace-nowrap group-hover:text-pink-500 ${pathname === item.href ? "text-general-pink" : ""}`}
-                      >
-                        {item.name}
-                      </span>
-                    </Link>
-                  )}
+              {/* Favorites Section */}
+              {favorites.map((fav) => (
+                <li key={fav.id} className="flex flex-col">
+                  <Link
+                    href={`/favorite/${fav.id}`}
+                    className="group flex items-center space-x-3 hover:bg-slate-400/20 p-2 rounded-md"
+                  >
+                    <FaHeart className="text-xs group-hover:text-pink-500" />
+                    <span className="sub-header whitespace-nowrap group-hover:text-pink-500">{fav.playlistName}</span>
+                  </Link>
                 </li>
               ))}
+
+              {/* Playlists Section */}
+              {playlists.map((playlist) => (
+                <li key={playlist.id} className="flex flex-col">
+                  <Link
+                    href={`/playlist/${playlist.id}`}
+                    className="group flex items-center space-x-3 hover:bg-slate-400/20 p-2 rounded-md"
+                  >
+                    <FaMusic className="text-xs group-hover:text-pink-500" />
+                    <span className="sub-header whitespace-nowrap group-hover:text-pink-500">
+                      {playlist.playlistName}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+
+              {/* Add Playlist Button */}
+              <li className="flex flex-col">
+                <div
+                  onClick={() => setIsAddPlaylistModalOpen(true)}
+                  className="group flex items-center space-x-3 hover:bg-slate-400/20 p-2 rounded-md cursor-pointer"
+                >
+                  <FaPlus className="text-xs group-hover:text-pink-500" />
+                  <span className="sub-header whitespace-nowrap group-hover:text-pink-500">Add Playlist</span>
+                </div>
+              </li>
             </ul>
           </div>
-        ))}
 
-        {/* Playlist and Favorites Section */}
-        <div className="mb-4">
-          <h3 className="sidebar-second-header tracking-wide mb-3 whitespace-nowrap">Playlist and Favorite</h3>
-          <ul>
-            {/* Favorites Section */}
-            {favorites.map((fav) => (
-              <li key={fav.id} className="flex flex-col">
-                <Link
-                  href={`/favorite/${fav.id}`}
-                  className="group flex items-center space-x-3 hover:bg-slate-400/20 p-2 rounded-md"
-                >
-                  <FaHeart className="text-xs group-hover:text-pink-500" />
-                  <span className="sub-header whitespace-nowrap group-hover:text-pink-500">{fav.playlistName}</span>
-                </Link>
-              </li>
-            ))}
-
-            {/* Playlists Section */}
-            {playlists.map((playlist) => (
-              <li key={playlist.id} className="flex flex-col">
-                <Link
-                  href={`/playlist/${playlist.id}`}
-                  className="group flex items-center space-x-3 hover:bg-slate-400/20 p-2 rounded-md"
-                >
-                  <FaMusic className="text-xs group-hover:text-pink-500" />
-                  <span className="sub-header whitespace-nowrap group-hover:text-pink-500">
-                    {playlist.playlistName}
-                  </span>
-                </Link>
-              </li>
-            ))}
-
-            {/* Add Playlist Button */}
-            <li className="flex flex-col">
-              <div
-                onClick={() => setIsAddPlaylistModalOpen(true)}
-                className="group flex items-center space-x-3 hover:bg-slate-400/20 p-2 rounded-md cursor-pointer"
-              >
-                <FaPlus className="text-xs group-hover:text-pink-500" />
-                <span className="sub-header whitespace-nowrap group-hover:text-pink-500">Add Playlist</span>
-              </div>
-            </li>
-          </ul>
+          {/* Add Playlist Modal */}
+          <AddPlaylistModal isOpen={isAddPlaylistModalOpen} onClose={() => setIsAddPlaylistModalOpen(false)} />
         </div>
-
-        {/* Add Playlist Modal */}
-        <AddPlaylistModal isOpen={isAddPlaylistModalOpen} onClose={() => setIsAddPlaylistModalOpen(false)} />
       </div>
-    </div>
+    </>
   );
 };
 
