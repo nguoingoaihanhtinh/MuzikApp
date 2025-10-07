2e0058f7ce96a272ee1b30a7d6ebdbffa99226cd
nguoingoaihanhtinh <22520688@gm.uit.edu.vn>
2025-10-07 11:22:11 +0700

    update window

---
+++ Muzik/Data/Migrations/20251007042024_WindowInitial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Muzik.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class WindowInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
